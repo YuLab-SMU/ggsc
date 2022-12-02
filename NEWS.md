46f2008de9d5b1bc5dbcce67b409f11ac2e7f9e1
Guangchuang Yu <guangchuangyu@gmail.com>
2022-12-02 16:12:21 +0800

    ellipse

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# scplot 0.0.2
+
++ `sc_dim_geom_ellipse` to draw ellipse on `sc_dim()` (2022-12-02, Fri)
+
 # scplot 0.0.1
 
 + `sc_dim`, `sc_dim_geom_feature`, `sc_dim_geom_label`, `sc_feature`, `sc_geom_point` and `sc_violin` functions (2022-11-09, Wed)
--- R/sc-dim-utilities.R
+++ R/sc-dim-utilities.R
@@ -44,13 +44,48 @@ ggplot_add.sc_dim_geom_label <- function(object, plot, object_name) {
 
     geom <- object$geom
     object$geom <- NULL
-    default_mapping <- aes_string(x="x", y = "y", label = "ident")
+    default_mapping <- aes(x=.data$x, y = .data$y, label = .data$ident)
     if (is.null(object$mapping)) {
         object$mapping <- default_mapping
     } else {
         object$mapping <- utils::modifyList(default_mapping, object$mapping)
     }
     ly <- do.call(geom, object)    
+    ggplot_add(ly, plot, object_name)
+}
+
+
+
+##' @title sc_dim_geom_ellipse
+##' @rdname sc-dim-geom-ellipse
+##' @param mapping, aesthetic mapping
+##' @param level the level at which to draw an ellipse
+##' @param ... additional parameters pass to the stat_ellipse
+##' @return layer of ellipse
+##' @seealso
+##'  [stat_ellipse][ggplot2::stat_ellipse]; 
+##' @export
+sc_dim_geom_ellipse <- function(mapping = NULL, level = 0.95, ...) {
+    structure(list(mapping = mapping, level = level, ...), class = "sc_dim_geom_ellipse")
+}
+
+##' @importFrom ggplot2 ggplot_add
+##' @importFrom rlang .data
+##' @method ggplot_add sc_dim_geom_ellipse
+##' @importFrom ggplot2 aes
+##' @importFrom ggplot2 stat_ellipse
+##' @export                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+ggplot_add.sc_dim_geom_ellipse <- function(object, plot, object_name) {
+    dims <- names(plot$data)[1:2]
+    default_mapping <- aes(x = .data[[dims[1]]], y = .data[[dims[2]]], group = .data$ident)
+    if (is.null(object$mapping)) {
+        mapping <- default_mapping
+    } else {
+        mapping <- modifyList(default_mapping, object$mapping)
+    }
+    object$mapping <- mapping
+
+    ly <- do.call(stat_ellipse, object)
     ggplot_add(ly, plot, object_name)
 }
 
