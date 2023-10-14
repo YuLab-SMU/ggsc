bc386f1d94f9f747757845a015382150c3158bb0
Guangchuang Yu <guangchuangyu@gmail.com>
2023-10-15 00:00:53 +0800

    package level man page

---
+++ R/ggsc-package.R
@@ -0,0 +1,14 @@
+#' Visualizing Single Cell Data
+#'
+#' 
+#' The package provides useful functions to visualize single cell and spatial 
+#'  data. It supports both 'SingleCellExperiment' and 'Seurat' objects.
+#'  It also supports visualizing the data using grammar of graphics 
+#'  implemented in 'ggplot2'.
+#'
+#' @name ggsc-package
+#' @aliases ggsc-package ggsc
+#' @docType package
+NULL
+
+
